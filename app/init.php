db250d806d0cb903aae306244fc294d4806c3083
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2009-03-26 20:17:33 +0000

    Updated init.php

--- app/init.php
+++ app/init.php
@@ -1,14 +1,14 @@
 <?php
 
-Registry::set("SITE","http://dev.assertis.net/svn/linus/xframe/");
+Registry::set("SITE","http://yourwebsite.com/");
 Registry::set("ADMIN","linusnorton@gmail.com");
 
 //Database settings
 Registry::set("DATABASE_ENGINE","MySQL");
-Registry::set("DATABASE_USERNAME", "root");
-Registry::set("DATABASE_PASSWORD", "XPMnwLk");
-Registry::set("DATABASE_HOST", "localhost");
-Registry::set("DATABASE_NAME", "cmsve");
+Registry::set("DATABASE_USERNAME", "");
+Registry::set("DATABASE_PASSWORD", "");
+Registry::set("DATABASE_HOST", "");
+Registry::set("DATABASE_NAME", "");
 
 //set up the object factory
 require_once(ROOT."app/.classes.php");
