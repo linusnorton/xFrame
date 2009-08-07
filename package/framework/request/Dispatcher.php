56ff373825d4fad60474692aefe68a7517e55cca
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2009-08-07 09:52:22 +0000

    Removed tabs, added getListeners to dispatcher

--- package/framework/request/Dispatcher.php
+++ package/framework/request/Dispatcher.php
@@ -8,7 +8,7 @@
  * This dispatcher stores a mapping of requests to handlers and dispatches requests to their correct handler
  */
 class Dispatcher {
-	private static $listeners = array();
+    private static $listeners = array();
 
     /**
      * This method takes the given request finds the request handler and passes the request to the handler
@@ -16,9 +16,9 @@ class Dispatcher {
      * @param Event $e
      * @return unknown
      */
-	public static function dispatch(Request $r) {
+    public static function dispatch(Request $r) {
 
-	    if (array_key_exists($r->getName(), self::$listeners)) {
+        if (array_key_exists($r->getName(), self::$listeners)) {
             $object = new self::$listeners[$r->getName()]["class"];
             $method = self::$listeners[$r->getName()]["method"];
 
@@ -30,7 +30,7 @@ class Dispatcher {
 
                 //if authorised do the request
                 if ($authResult === true) {
-        	        return $object->$method($r);
+                    return $object->$method($r);
                 }
                 //if not then forbidden
                 else if ($authResult === false) {
@@ -44,30 +44,30 @@ class Dispatcher {
             }
             //if no authenticator then just do it
             else {
-        	    return $object->$method($r);
+                return $object->$method($r);
             }
         }
-	    else {
-	        throw new UnknownRequest("No handler for ".$r->getName());
+        else {
+            throw new UnknownRequest("No handler for ".$r->getName());
         }
     }
 
-	/**
-	 * Ok this registers a method to call for a given a request
-	 *
-	 * @param String $request
-	 * @param String $class
-	 * @param String $method
+    /**
+     * Ok this registers a method to call for a given a request
+     *
+     * @param String $request
+     * @param String $class
+     * @param String $method
      * @param int $cacheLength
      * @param array $parameterMap
 	 */
-	public static function addListener($requestName, $class, $method, $cacheLength = false, array $parameterMap = array(), $authenticator = null) {
-	    self::$listeners[$requestName] = array("class" => $class,
-                                               "method" => $method,
-                                               "cacheLength" => $cacheLength,
-                                               "parameterMap" => $parameterMap,
-                                               "authenticator" => $authenticator);
-	}
+    public static function addListener($requestName, $class, $method, $cacheLength = false, array $parameterMap = array(), $authenticator = null) {
+        self::$listeners[$requestName] = array("class" => $class,
+                                           "method" => $method,
+                                           "cacheLength" => $cacheLength,
+                                           "parameterMap" => $parameterMap,
+                                           "authenticator" => $authenticator);
+    }
 
     /**
      * get the cache length for the given request
@@ -75,16 +75,23 @@ class Dispatcher {
      * @param $request Request to get the cache length for
      */
     public static function getCacheLength(Request $r) {
-	    if (array_key_exists($r->getName(), self::$listeners)) {
+        if (array_key_exists($r->getName(), self::$listeners)) {
 	        return self::$listeners[$r->getName()]["cacheLength"];
         }
-	    else {
-	       return false;
+        else {
+           return false;
         }
     }
 
     public static function getParameterMap($requestName) {
         return self::$listeners[$requestName]["parameterMap"];
+    }
+
+    /**
+     * return array
+     */
+    public static function getListeners() {
+        return self::$listeners;
     }
 }
 
