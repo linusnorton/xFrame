beb160869fca828c4bdbb2654ef37068c2df54f0
dominic@rewrite3.com <dominic@rewrite3.com@043552ae-18c8-11de-82b5-194b17410922>
2009-09-22 13:44:14 +0000

    ErrorCodes.php: Specify the code to description resource e.g. database, xml, csv file RedirectError: Add support for unkown error codes

--- package/framework/util/ErrorCodes.php
+++ package/framework/util/ErrorCodes.php
@@ -1,22 +1,38 @@
 <?php
 /**
- * Description of ErrorCodes
+ * Match error codes thrown by exceptions to an alternate description.
  *
- * @author Dominic Webb <dominic@rewrite3.com>
+ * An optional trigger will be returned. Usefull for indicating that something
+ * should be triggered by this alternate description.
+ *
+ * @author Dominic Webb <dominic.webb@assertis.co.uk>
  */
 class ErrorCodes {
-    
-    public static function getDesc ($code) {
 
-            $sql = "SELECT `desc` FROM error_codes WHERE `code` = :code ;";
+    /**
+     * Get the alternate description for an error code
+     * @param integer $code The Exception code that was thrown
+     * @param resource  The resource type for doing the code lookup e.g. database, xml file or csv file. Defaults to database
+     * @return array $result
+     *
+     * @todo add code for parsing an XML and CSV file containing the code to alt
+     *  descriptions
+     */
+    public static function getDesc ($code, $resource="db") {
+
+        if ($resource == "db") {
+            $sql = "SELECT `desc`, `trigger` FROM error_codes WHERE `code` = :code LIMIT 1;";
             $stmt = DB::dbh()->prepare($sql);
             $stmt->bindValue(":code", $code);
             $stmt->execute();
+            $result = $stmt->fetchAll();
+            return $result[0];
 
-            $res = $stmt->fetchAll();
-
-            return $res[0]['desc'];
-
+        } elseif ($resource == "xml") {
+            return null;
+        } elseif ($resource == "csv") {
+            return null;
+        }
     }
 }
 ?>
--- package/framework/util/RedirectError.php
+++ package/framework/util/RedirectError.php
@@ -1,35 +1,51 @@
 <?php
 /**
- *
+ * Persist errors in XML accross page views
+ * 
  * @author Dominic Webb <dominic.webb@assertis.co.uk>
  */
-class RedirectError implements XML {
+class RedirectError {
 
+
+    /**
+     * Add an error code
+     * @param integer $error
+     */
     public static function add($error) {      
         $_SESSION['error_redirect'][] = $error;
     }
 
+
+    /**
+     * Determines if there are errors that have been set
+     * @return boolean
+     */
     public static function hasErrors () {
         return (isset($_SESSION['error_redirect']));
     }
 
+
+    /**
+     *  Builds the error XML and unsets the persisted errors. Always returns true
+     *
+     * @return true
+     */
     public static function buildErrors () {
 
-        
         while (list($key, $val) = each($_SESSION['error_redirect'])) {
-//var_dump($val);die();
-//var_dump($_SESSION['error_redirect']); die();
-           $desc = ErrorCodes::getDesc($val);
-           $xml .= "<error code=\"{$val}\">{$desc}</error>";
+           
+           $code = ErrorCodes::getDesc($val);
+
+           if (empty($code['desc'])) {
+               $code['desc'] = "Unkown error code descrption '{$val}'";
+           }
+
+           $xml .= "<error code=\"{$val}\">{$code['desc']}</error>";
         }
         Page::addXML("<user-error>{$xml}</user-error>");
         unset($_SESSION['error_redirect']);
+        return true;
     }
 
-    public function getXML () {}
-
-    public function __destructor () {
-        
-    }
 }
 ?>
