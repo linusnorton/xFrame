e36c36066d01f8d9d792e1b7c42118fdeb6e98ab
dominic@rewrite3.com <dominic@rewrite3.com@043552ae-18c8-11de-82b5-194b17410922>
2009-10-29 16:48:55 +0000

    can validate postcodes on format and alpha numeric sequences

--- package/framework/util/InputValidator.php
+++ package/framework/util/InputValidator.php
@@ -115,7 +115,7 @@ class InputValidator {
 
     public static function isPostCode($postCode) {
         $pattern = '/^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/';
-        return (preg_match($postCode, $email));
+        return (preg_match($postCode, $pattern));
     }
 }
 ?>
