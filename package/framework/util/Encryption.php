5fb6b71d120bb5b9118bfae5f70dae1a11dd08d8
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2009-11-18 12:24:11 +0000

    Updated the Encryption class to clarify the difference between using a hex string and a long

--- package/framework/util/Encryption.php
+++ package/framework/util/Encryption.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @author Linus Norton <linusnorton@gmail.com>
- *
  * @package util
  *
  * Object wrapper for some mcrypt functions. This requires the mcrypt and mhash extentions
@@ -22,24 +21,43 @@ class Encryption {
      * Create the initialization vector
      */
     public function createIV() {
-        $iv_size = mcrypt_get_iv_size($this->algorithm, $this->mode);
-        return mcrypt_create_iv($iv_size, $this->mode);
+        $ivSize = mcrypt_get_iv_size($this->algorithm, $this->mode);
+        return mcrypt_create_iv($ivSize, MCRYPT_DEV_URANDOM);
     }
 
     /**
-     * encrypt the given string using the key and iv
+     * encrypt the given string using the key and iv return the HEX string
+     *
+     * Use this method if you want to get a hex string back
+     */
+    public function encryptString($plainText, $key, $iv) {
+        return bin2hex(mcrypt_encrypt($this->algorithm, $key, $plainText, $this->mode, $iv));
+    }
+
+    /**
+     * decrypt the given HEX string using the key and iv
+     *
+     * Use this method if you are passing through a hex string
+     */
+    public function decryptString($encryptedText, $key, $iv) {
+        $decryptedText = mcrypt_decrypt( $this->algorithm , $key  , pack("H*",$encryptedText ) , $this->mode , $iv);
+        return str_replace("\x0", '', $decryptedText);
+    }
+
+    /**
+     * encrypt the given string using the key and iv return the long
      */
     public function encrypt($plainText, $key, $iv) {
-        return bin2hex(mcrypt_encrypt($this->algorithm, $key, $plainText, $this->mode, pack("H*",$iv)));
+        return mcrypt_encrypt($this->algorithm, $key, $plainText, $this->mode, $iv);
     }
 
     /**
-     * decrypt the given string using the key and iv
+     * decrypt the given long using the key and iv
      */
     public function decrypt($encryptedText, $key, $iv) {
-        return str_replace("\x0", '', mcrypt_decrypt( $this->algorithm , $key  , pack("H*",$encryptedText ) , $this->mode , pack("H*",$iv)));
+        $decryptedText = mcrypt_decrypt($this->algorithm , $key, $encryptedText , $this->mode, $iv);
+        return str_replace("\x0", '', $decryptedText);
     }
 
 }
 
-?>
