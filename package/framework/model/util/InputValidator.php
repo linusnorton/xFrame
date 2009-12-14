d744d3b7c47027fae3150e5d79c46b4fa534dc3f
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2009-12-14 11:23:27 +0000

    Looks like someone added a comment but forgot the actual code

--- package/framework/model/util/InputValidator.php
+++ package/framework/model/util/InputValidator.php
@@ -112,4 +112,7 @@ class InputValidator {
      * @param <type> $postCode
      * @return boolean
      */
+    public static function isPostCode($input) {
+        return preg_match("/^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/", $input);
+    }
 }
