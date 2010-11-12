1c00498981580f13e670f72bb227f0ea8720e034
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2010-11-12 22:37:06 +0000

    Updated the framework bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,12 +1,32 @@
 <?php
+/**
+ * Boot the framework before we start running the tests.
+ *
+ * Please note that this bootstrap file requires PHPUnit 3.5 or later.
+ *
+ * Remove the whitelist and blacklist lines to make backwards compatible.
+ */
 
-define("ROOT", "./package/");
-ini_set("include_path", ini_get("include_path").":".ROOT);
-include("framework/init.php");
+$_SESSION = array();
+
+foreach ($_SERVER["argv"] as $arg) {
+    if (strpos($arg, "XFRAME_CONFIG") !== false) {
+        $parts = explode("=", $arg);
+        $_SERVER["XFRAME_CONFIG"] = $parts[1];
+    }
+}
+
+//running from netbeans
+if ($_SERVER["XFRAME_CONFIG"] == "") {
+    $_SERVER["XFRAME_CONFIG"] = "../config/dev.ini";
+}
+
+set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__)."/../../");
+require_once 'xframe/init.php';
 
 // Don't include in the coverage report any files under the test directory.
-PHPUnit_Util_Filter::addDirectoryToFilter(ROOT.'framework/test');
-PHPUnit_Util_Filter::addDirectoryToFilter(ROOT.'framework/model/util/phpmailer');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(APP_DIR.'test');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(APP_DIR.'model/util/phpmailer');
 // Whitelist
-PHPUnit_Util_Filter::addDirectoryToWhitelist(ROOT.'framework/model');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToWhitelist(APP_DIR.'model');
 
