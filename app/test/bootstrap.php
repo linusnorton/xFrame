e9f76801a0b29bce3b321dbdca1922fa4853ed0e
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2010-11-12 22:38:59 +0000

    Updated build script and bootstrap for PHPUnit 3.5

--- app/test/bootstrap.php
+++ app/test/bootstrap.php
@@ -1,10 +1,33 @@
 <?php
 
-define("ROOT", "./package/");
-include(ROOT."framework/init.php");
+/**
+ * Boot the framework before we start running the tests.
+ *
+ * Please note that this bootstrap file requires PHPUnit 3.5 or later.
+ *
+ * Remove the whitelist and blacklist lines to make backwards compatible.
+ */
+
+$_SESSION = array();
+
+foreach ($_SERVER["argv"] as $arg) {
+    if (strpos($arg, "XFRAME_CONFIG") !== false) {
+        $parts = explode("=", $arg);
+        $_SERVER["XFRAME_CONFIG"] = $parts[1];
+    }
+}
+
+//running from netbeans
+if ($_SERVER["XFRAME_CONFIG"] == "") {
+    $_SERVER["XFRAME_CONFIG"] = "../config/dev.ini";
+}
+
+set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__)."/../../");
+require_once 'xframe/init.php';
+
 
 // Don't include in the coverage report any files under the test directory.
-PHPUnit_Util_Filter::addDirectoryToFilter(APP_DIR.'test');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(APP_DIR.'test');
 // Whitelist
-PHPUnit_Util_Filter::addDirectoryToWhitelist(APP_DIR.'controller');
-PHPUnit_Util_Filter::addDirectoryToWhitelist(APP_DIR.'model');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToWhitelist(APP_DIR.'controller');
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToWhitelist(APP_DIR.'model');
