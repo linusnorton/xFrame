5aebdf92972687b1fe42f7ee4e30a2fe6b673e81
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2011-02-01 10:03:31 +0000

    Added the Dependency Injection Container

---
+++ lib/xframe/core/DependencyInjectionContainer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace xframe\core;
+
+/**
+ * Used to store the applications dependencies.
+ *
+ * @author Linus Norton <linusnorton@gmail.com>
+ */
+class DependencyInjectionContainer extends Container {
+
+    /**
+     * Stores the lambda functions that get the dependencies
+     * @var array
+     */
+    protected $builders;
+    
+    /**
+     * Constructor
+     */
+    public function __construct(array $attributes = array(),
+                                array $builders = array()) {
+        parent::__construct($attributes);
+        $this->builders = $builders;
+    }
+
+    /**
+     * Add a lambda function that returns a dependency
+     *
+     * @param string $name
+     * @param callback $lambda
+     */
+    public function add($name, $lambda) {
+        $this->builders[$name] = $lambda;
+    }
+
+    /**
+     * If the requested dependency has not been set, if we have a lambda
+     * to create it do so then return the dependency
+     * 
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name) {
+        if (!isset($this->attributes[$name]) && isset($this->builders[$name])) {
+            $this->attributes[$name] = $this->builders[$name]($this);
+        }
+
+        return $this->attributes[$name];
+    }
+
+}
