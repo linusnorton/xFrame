2dc11be176ceed8bd6be52362093ddc0c1e84b48
linusnorton <linusnorton@043552ae-18c8-11de-82b5-194b17410922>
2011-03-13 16:06:57 +0000

    Added basic DBAuth

---
+++ lib/xframe/authentication/DBAuth.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace xframe\authentication;
+
+/**
+ * Uses a PDO connect to auth given credentials
+ *
+ * @author Linus Norton <linusnorton@gmail.com>
+ * @package authentication
+ */
+class DBAuth implements Authenticator {
+    private $pdo;
+    private $table;
+    private $identityColumn;
+    private $credentialColumn;
+    
+    /**
+     * @param PDO $pdo
+     * @param string $table
+     * @param string $identityColumn
+     * @param string $credentialColumn 
+     */
+    function __construct(\PDO $pdo, $table, $identityColumn, $credentialColumn) {
+        $this->pdo = $pdo;
+        $this->table = $table;
+        $this->identityColumn = $identityColumn;
+        $this->credentialColumn = $credentialColumn;
+    }
+
+    /**
+     * @param string $identity
+     * @param string $credential
+     * @return boolean
+     */
+    public function authenticate($identity, $credential) {
+        $stmt = $pdo->prepare(
+            "SELECT * FROM `{$this->table}` 
+             WHERE `{$this->identityColumn}` = :identity
+             AND `{$this->credentialColumn}` = :credential
+             LIMIT 1"
+        );
+                               
+        $stmt->bindValue("identity", $identity);
+        $stmt->bindValue("credential", $credential);
+        $stmt->execute();
+        
+        return $stmt->numResults() > 0;
+    }
+}
+
