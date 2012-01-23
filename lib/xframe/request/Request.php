142d7ffb617fad63f8478fbc5cb7f46d28811bd5
Linus Norton <linusnorton@gmail.com>
2012-01-23 07:39:06 +0000

    Tidied up the Request and View classes (they now extend Container), added support for PHPView

--- lib/xframe/request/Request.php
+++ lib/xframe/request/Request.php
@@ -2,6 +2,7 @@
 
 namespace xframe\request;
 use \Exception;
+use \xframe\util\Container;
 
 /**
  * @author Linus Norton <linusnorton@gmail.com>
@@ -10,9 +11,8 @@ use \Exception;
  * This encapsulates a given request. Usually this object will be routed
  * through the front controller and handled by a request controller
  */
-class Request {
+class Request extends Container {
     private $requestedResource;
-    private $parameters;
     private $mappedParameters;
 
     public $files;
@@ -38,7 +38,8 @@ class Request {
         //get the parameters out of the request URI
         $this->mappedParameters = array_slice($request, 1);
         //store the other params (usually from $_REQUEST)
-        $this->parameters = $parameters;        
+        parent::__construct($parameters);
+
         //get the $_FILES array
         $this->files = &$_FILES;
         $this->cookie = &$_COOKIE;
@@ -70,7 +71,7 @@ class Request {
             $parameter->validate($this->mappedParameters[$i]);  
             
             // add the parameter
-            $this->parameters[$parameter->getName()] = $this->mappedParameters[$i];
+            $this->attributes[$parameter->getName()] = $this->mappedParameters[$i];
         }
     }
     
@@ -84,8 +85,8 @@ class Request {
     /**
      * @param array $parameters 
      */
-    public function setMappedParameters(array $parameters) {
-        $this->mappedParameters = $parameters;
+    public function setMappedParameters(array &$parameters) {
+        $this->mappedParameters = &$parameters;
     }
 
     /**
@@ -96,52 +97,6 @@ class Request {
     }
 
     /**
-     * @return array
-     */
-    public function getParameters() {
-        return $this->parameters;
-    }
-
-    /**
-     * Magic function overload. Allows east access to the request parameters.
-     *
-     * @param mixed $key
-     * @return mixed
-     */
-    public function __get($key) {
-        if (array_key_exists($key, $this->parameters)) {
-            return $this->parameters[$key];
-        }
-    }
-
-    /**
-     * Magic function overload. Allows easy access to the request parameters
-     *
-     * @param mixed $key
-     * @param mixed $value
-     */
-    public function __set($key, $value) {
-        $this->parameters[$key] = $value;
-    }
-
-    /**
-     * Unset the given variable
-     * 
-     * @param mixed $key
-     */
-    public function __unset($key) {
-        unset($this->parameters[$key]);
-    }
-
-    /**
-     * @param $key
-     * @return boolean
-     */
-    public function __isset($key) {
-        return isset($this->parameters[$key]);
-    }
-
-    /**
      * Return a hash of the Request
      */
     public function hash() {
@@ -149,5 +104,4 @@ class Request {
                    implode($this->parameters).
                    implode(array_keys($this->parameters)));
     }
-
 }
