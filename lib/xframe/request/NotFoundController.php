ba0d817420ea8d89da49b87541698f5ca05d8f33
Donatas Martinkus <donatasmart@gmail.com>
2013-12-30 23:54:37 +0000

    fixing privacy levels which were left after rolling back NotFoundController class extention

--- lib/xframe/request/NotFoundController.php
+++ lib/xframe/request/NotFoundController.php
@@ -9,7 +9,7 @@ namespace xframe\request;
  */
 class NotFoundController extends Controller {
 
-    private $request;
+    protected $request;
 
     public function __construct(Request $request) {
         $this->request = $request;
